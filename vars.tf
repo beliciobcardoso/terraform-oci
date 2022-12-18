fa4db81f91036c0f8e9ee01200d05a0fb9373238
Belicio <belloinfo@gmail.com>
2022-12-18 16:03:31 +0000

    comiite

---
+++ vars.tf
@@ -0,0 +1,6 @@
+variable compartment_ocid { default = "ocid1.tenancy.oc1..aaaaaaaae4w7y3jpdhmojsky5azv5ywoiqbptvrajmpnfavu4rt2aupfgi4a" }
+variable tenancy_ocid { default = "ocid1.tenancy.oc1..aaaaaaaae4w7y3jpdhmojsky5azv5ywoiqbptvrajmpnfavu4rt2aupfgi4a" }
+variable oci_core_instance--source_details-source_id--export_SRV-belloinfo_1 { default = "ocid1.image.oc1.sa-saopaulo-1.aaaaaaaa6enqtds2brgrjophs2oce74ez7bas5kmwb473fbzl7qhdhoca5iq" }
+variable availability_domain--ArmX-SA-SAOPAULO-1-AD-1 { default = "ArmX:SA-SAOPAULO-1-AD-1" }
+variable fault_domain--FAULT-DOMAIN-3 { default = "FAULT-DOMAIN-3" }
+variable region { default = "sa-saopaulo-1" }
